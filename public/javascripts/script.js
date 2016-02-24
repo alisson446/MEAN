5414008a07816337293272190a361302b3636186
alisson oliveira <alissonoliveira446@gmail.com>
2016-02-24 11:54:43 -0300

    Projeto mean

---
+++ public/javascripts/script.js
@@ -0,0 +1,59 @@
+angular.module('mean', []);
+
+angular.module('mean').controller('ClienteController', function($scope, $http) {
+
+	$http.get('/todos').success(function(retorno) {
+		$scope.clientes = retorno.clientes;
+	});
+
+	$scope.show = function(view, id) {
+		$scope.modal = true;
+		$scope.cliente = null;
+
+		if(view == 'novo') {
+			$scope.tituloModal = 'Cadastro';
+		}else {
+			$scope.tituloModal = 'Edição';
+			exibirEdicao(id);
+		}
+	};
+
+	$scope.salvar = function(cliente) {
+		$http.post('/salvar', cliente).success(function(retorno) {
+			$scope.clientes.push(retorno.cliente);
+			$scope.cliente = null;
+		});
+	};
+
+	var exibirEdicao = function(id) {
+		var idObject = {idObj: id};
+
+		$http.post('/exibir', idObject).success(function(retorno) {
+			$scope.cliente = retorno.cliente;
+		});
+	};
+
+	$scope.editar = function(cliente) {
+		$http.post('/editar', cliente).success(function() {
+			for(var i in $scope.clientes) {
+				if($scope.clientes[i]._id == cliente._id) {
+					$scope.clientes[i].nome = cliente.nome;
+					$scope.clientes[i].idade = cliente.idade;
+				}
+			}
+		});
+	};
+
+	$scope.deletar = function(id) {
+		var idObject = {idObj: id};
+
+		$http.post('/deletar', idObject).success(function() {
+			for(var i in $scope.clientes) {
+				if($scope.clientes[i]._id == id) {
+					$scope.clientes.splice(i, 1);
+				}
+			}
+			$scope.modal = false;
+		});
+	};
+});
---
+++ routes/index.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+/* GET home page. */
+router.get('', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+var db = mongoose.connect('mongodb://localhost/mean');
+var Schema = mongoose.Schema;
+
+var clienteSchema = new Schema({
+	nome: {type: String, required: true},
+	idade: {type: String, required: true}
+});
+
+var Cliente = db.model('Cliente', clienteSchema, 'collection');
+
+router.todos = function(req, res) {
+	Cliente.find({}, function(error, clientes) {
+		if(error) {
+			res.send(500);
+ 		}else {
+			res.json({ clientes: clientes });
+		}
+	});
+};
+
+router.salvarCliente = function(req, res) {
+	var cliente = new Cliente(req.body);
+	cliente.save(function(error, clienteCadastrado) {
+		if(error) {
+			res.send(500);
+		}else {
+			res.json({cliente: clienteCadastrado});
+		}
+	});
+};
+
+router.exibir = function(req, res) {
+	var id = req.body.idObj.toString();
+
+	Cliente.findOne({_id: id}, function(error, cliente) {
+		if(error) {
+			res.send(500);
+ 		}else {
+			res.json({cliente: cliente});
+		}
+	});
+};
+
+router.editar = function(req, res) {
+	Cliente.findOneAndUpdate({_id: req.body._id}, req.body, function(error, cliente) {
+		if(error) {
+			res.send(500);
+ 		}else {
+			res.send(201);
+		}
+	});
+};
+
+router.deletar = function(req, res) {
+	var id = req.body.idObj.toString();
+
+	Cliente.remove({_id: id}, function(error) {
+		if(error) {
+			res.send(500);
+ 		}else {
+			res.send(201);
+		}
+	});
+};
+
+module.exports = router;
